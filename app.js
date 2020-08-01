3730aa1c8531e791eb17edc289500c6003c816aa
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-01 23:56:00 +0200

    Changed: First and second page of the scroll

--- app.js
+++ app.js
@@ -8,20 +8,29 @@ new fullpage("#fullpage", {
     const section = destination.item;
     const title = section.querySelector("h1");
     const tl = new TimelineMax({ delay: 0.5 });
+
     tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
+
     if (destination.index === 1) {
-      const chairs = document.querySelectorAll(".chair");
+      const sneackers = document.querySelectorAll(".sneacker");
       const description = document.querySelector(".description");
-      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-10%" })
+
+      tl.fromTo(sneackers, 0.7, { x: "130%" }, { x: "-10%" })
         .fromTo(
           description,
           0.5,
           { opacity: 0, y: "50" },
           { y: "0", opacity: 1 }
-        )
-        .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
-        .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
-        .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+        );
+
+        const t2 =  new TimelineMax({delay: 1.2});
+    
+        t2.fromTo(sneackers[0], 1, { opacity: 1 }, { opacity: 1 })
+        .fromTo(sneackers[1], 1, { opacity: 0 }, { opacity: 1 })
+        .fromTo(sneackers[2], 1, { opacity: 0 }, { opacity: 1 });
     }
+
+
+
   }
 });
