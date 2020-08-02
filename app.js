eeab8efda3283e28a609cf11dde0f5290df3fc18
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-02 07:27:19 +0200

    Added: ctb div on page 3

--- app.js
+++ app.js
@@ -21,16 +21,37 @@ new fullpage("#fullpage", {
           0.5,
           { opacity: 0, y: "50" },
           { y: "0", opacity: 1 }
-        );
-
-        const t2 =  new TimelineMax({delay: 1.2});
-    
-        t2.fromTo(sneackers[0], 1, { opacity: 1 }, { opacity: 1 })
+        )
+        .fromTo(sneackers[0], 1, { opacity: 1 }, { opacity: 1 })
         .fromTo(sneackers[1], 1, { opacity: 0 }, { opacity: 1 })
         .fromTo(sneackers[2], 1, { opacity: 0 }, { opacity: 1 });
+    } else if (destination.index === 2) {
+      const ctbContainer = document.querySelector(".ctb");
+
+      tl.fromTo(
+        ctbContainer,
+        0.5,
+        {
+          height: 5,
+          width: 0,
+          opacity: 0,
+        },
+        {
+          width: "50%",
+          opacity: 1,
+        }
+      ).fromTo(
+        ctbContainer,
+        0.8,
+        {
+          width: "50%",
+          height: 5,
+        },
+        {
+          height: "50%",
+          width: "50%",
+        }
+      );
     }
-
-
-
-  }
+  },
 });
